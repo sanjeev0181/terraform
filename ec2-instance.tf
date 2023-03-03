61bfff3780181e20311ab5a4f6036a5727e00367
chaan2835 <92902854+chaan2835@users.noreply.github.com>
2023-03-03 16:22:11 +0530

    added some more
    instance created, created security group, added vpc, made ssh connection, creating s3 buckets in aws, uploading single and multiple files to s3 buckets, creating key-pair using tf script

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -18,8 +18,8 @@ provider "aws" {
 resource "aws_instance" "test" {
   ami           = "ami-0557a15b87f6559cf"
   instance_type = "t2.micro"
-
-# adding the security group to instance
+  key_name      = "id_rsa.pub"
+  # adding the security group to instance
 
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   tags = {
@@ -29,16 +29,17 @@ resource "aws_instance" "test" {
 
 # creating security group
 resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
+  name = "allow_tls"
+
   description = "Allow TLS inbound traffic"
   # vpc_id      = aws_vpc.main.id
 
   ingress {
-    description      = "TLS from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
 
   }
 
@@ -55,7 +56,39 @@ resource "aws_security_group" "allow_tls" {
   }
 }
 
-# creating aws key pair 
+#making ssh connection
+resource "aws_key_pair" "deployer" {
+  key_name   = "id_rsa.pub"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+
+#creating s3 bucket in aws
+
+resource "aws_s3_bucket" "first"{
+    bucket = "simple-tf-b.0.1"
+
+}
+
+#uploding the files to s3-buckets
+
+resource "aws_s3_bucket_object" "buckets_s3v"{
+    bucket = aws_s3_bucket.first.id
+    key = "terraform.tfstate"
+    source ="/home/ubuntu/terraform-practise/terraform.tfstate"
+
+}
+
+# uploding multiple files
+
+resource "aws_s3_bucket_object" "multiples3files"{
+   bucket =aws_s3_bucket.first.id
+   for_each=fileset("/home/ubuntu/tf-dummy-files","*")
+   key = each.value
+   source ="/home/ubuntu/tf-dummy-files/${each.value}"
+}
+/*#creating key-pair using tf script
+
 resource "aws_key_pair" "TF_key" {
   key_name   = "TF_key"
   public_key = tls_private_key.rsa.public_key_openssh
@@ -69,4 +102,4 @@ resource "tls_private_key" "rsa" {
 resource "local_file" "TF_key" {
   content  = tls_private_key.rsa.private_key_pem
   filename = "tfkey"
-}
+}*/
