5899a2e7e6c5e2cbce97c3d71890d76ed2b882b6
Chebrolu Chandra Sekhar <Sekhar.Chebrolu@ivycomptech.com>
2023-03-14 11:31:21 +0530

    adding keypair

--- single-instance/using-resource/adding-keypair.tf
+++ single-instance/using-resource/adding-keypair.tf
@@ -3,5 +3,5 @@
 #making ssh connection
 resource "aws_key_pair" "deployer" {
   key_name   = "n.v-cnv"
-  public_key = file("C:\Users\sekhar.chebrolu\Downloads\dimpu\chan\devops\n.v-cnv.pem")
+  public_key = file("/private_key/n.v-cnv.pem")
 }
