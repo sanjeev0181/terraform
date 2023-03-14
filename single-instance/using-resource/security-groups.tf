8b3732d53226a8e58dc9071737bd11eb913bd595
Chebrolu Chandra Sekhar <Sekhar.Chebrolu@ivycomptech.com>
2023-03-14 11:13:22 +0530

    added sg

--- single-instance/using-resource/security-groups.tf
+++ single-instance/using-resource/security-groups.tf
@@ -1,31 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-
-}
-
-# creating instance
-resource "aws_instance" "ec2-instance-sg" {
-  ami           = "ami-0557a15b87f6559cf"
-  instance_type = "t2.micro"
- 
-  # adding the security group to instance
-
-  vpc_security_group_ids = [aws_security_group.allowing_sg.id]
-  tags = {
-    Name = "instance-tf-1"
-  }
-}
-
 # creating security group
 resource "aws_security_group" "allowing_sg" {
   name = "allowing_sg"
