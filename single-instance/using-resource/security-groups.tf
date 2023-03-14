b46c144ebf2b3106e14b0326b9b614abc9f1445b
Chebrolu Chandra Sekhar <Sekhar.Chebrolu@ivycomptech.com>
2023-03-14 11:12:15 +0530

    added sg

--- single-instance/using-resource/security-groups.tf
+++ single-instance/using-resource/security-groups.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 # creating instance
-resource "aws_instance" "ec2-instance" {
+resource "aws_instance" "ec2-instance-sg" {
   ami           = "ami-0557a15b87f6559cf"
   instance_type = "t2.micro"
  
