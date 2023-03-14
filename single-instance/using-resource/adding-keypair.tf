c0df9150cecf8c587c2a0a78af57e13ca07e7d5a
Chebrolu Chandra Sekhar <Sekhar.Chebrolu@ivycomptech.com>
2023-03-14 11:39:26 +0530

    adding keypair

--- single-instance/using-resource/adding-keypair.tf
+++ single-instance/using-resource/adding-keypair.tf
@@ -2,6 +2,6 @@
 
 #making ssh connection
 resource "aws_key_pair" "deployer" {
-  key_name   = "n.v-cnv"
+  key_name   = "n.v_key"
   public_key = file("/home/ubuntu/terraform/terraform/single-instance/using-resource//private_key/n.v-cnv.pem")
 }
