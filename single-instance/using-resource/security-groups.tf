9153a0de25f4b7d23c00ae2dc6b45eed29ad8ffe
Chebrolu Chandra Sekhar <Sekhar.Chebrolu@ivycomptech.com>
2023-03-15 15:54:15 +0530

    added nginx port num in sg.tf

--- single-instance/using-resource/security-groups.tf
+++ single-instance/using-resource/security-groups.tf
@@ -14,6 +14,15 @@ resource "aws_security_group" "allowing_sg" {
 
   }
 
+  ingress {
+    description = "nginx-portnum"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
