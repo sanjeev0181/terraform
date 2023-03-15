c25e8a63bf508be39c4a93b8b9df178a7050d469
Chebrolu Chandra Sekhar <Sekhar.Chebrolu@ivycomptech.com>
2023-03-15 16:16:46 +0530

    added sample.sh

--- single-instance/using-resource/ec2-instance.tf
+++ single-instance/using-resource/ec2-instance.tf
@@ -22,13 +22,9 @@ resource "aws_instance" "ec2-instance" {
   vpc_security_group_ids = [aws_security_group.allowing_sg.id]
 
   #sending some data using user-data 
-  user_data = <<-EOF
-    #!/bin/bash
-    sudo apt-get update
-    sudo apt-get install -y nginx
-    sudo systemctl enable nginx
-    sudo systemctl start nginx
-  EOF
+  user_data = "${file("sample.sh")}"
+    
+  
 
   # adding key-pair 
   key_name      = "id_rsa.pub"
