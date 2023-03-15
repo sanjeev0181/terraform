c0bb01b15a5980afd24ad07dfd2e470bba371351
Chebrolu Chandra Sekhar <Sekhar.Chebrolu@ivycomptech.com>
2023-03-15 16:10:14 +0530

    installing nginx in mumbai region

--- single-instance/using-resource/ec2-instance.tf
+++ single-instance/using-resource/ec2-instance.tf
@@ -9,14 +9,14 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 
 }
 
 
 # creating instance
 resource "aws_instance" "ec2-instance" {
-  ami           = "ami-0557a15b87f6559cf"
+  ami           = "ami-0f8ca728008ff5af4"
   instance_type = "t2.micro"
   # adding the security group to instance
   vpc_security_group_ids = [aws_security_group.allowing_sg.id]
