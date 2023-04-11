3c44471f17a5e4965e9f61f1d98dff801d857d4a
sanjeev0181 <Adityasanjeev1996@gmail.com>
2023-04-11 17:59:32 +0530

    adding to ec2 instance

---
+++ ec2-instance.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  access_key = "AKIAXSUFX6EHGB6OOINC"
+  secret_key = "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"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-007855ac798b5175e"
+  instance_type = "t2.micro"
+  user_data = <<EOF
+#!/bin/bash
+sudo apt update 
+sudo apt install nginx -y
+EOF
+    }
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
